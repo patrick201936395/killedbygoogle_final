7256c7aff586f1a90d33170dd5c6d9a7a6439516
Patrick Ruan <66738752+patrick201936395@users.noreply.github.com>
2025-12-24 12:27:17 +0800

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/Killedbygoogle/',
+    base: '/killedbygoogle_final/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
